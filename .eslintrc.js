570983382ae29dd1a42e8672ac5fefd55cf5801c
Parth Kalsi <kalsiparth252@gmail.com>
2024-07-27 13:16:31 +0530

    updated dependencie

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,15 +4,23 @@ module.exports = {
       es2021: true
     },
     extends: [
-      "eslint:recommended",
-      "plugin:react/recommended"
+      'eslint:recommended',
+      'plugin:react/recommended'
     ],
     parserOptions: {
       ecmaVersion: 12,
-      sourceType: "module"
+      sourceType: 'module'
     },
+    plugins: [
+      'react'
+    ],
     rules: {
-      // custom rules
+      // Your custom rules here
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
     }
   };
   
